0b535c0dc0901cce6d43fad35be1099860b90311
Raphael B. Dias <86736451+raphaelbdias@users.noreply.github.com>
2024-06-29 18:34:08 +0000

    new script

--- solutions/queries.sql
+++ solutions/queries.sql
@@ -12,5 +12,25 @@ GROUP BY
 HAVING 
     SUM(s.salesAmount) IS NOT NULL
 ORDER BY
-    SUM(s.salesAmount) DESC
-;
+    SUM(s.salesAmount) DESC;
+
+-- Query 2: Find top Sales person
+
+SELECT 
+    e.firstName || ' ' || e.lastName AS employee_name,
+    SUM(s.salesAmount) AS total_sales
+FROM
+    employee AS e
+LEFT JOIN
+    sales AS s
+ON
+    e.employeeId = s.employeeId
+GROUP BY
+    employee_name
+HAVING
+    total_sales IS NOT NULL
+ORDER BY
+    total_sales DESC
+LIMIT 
+    5;
+
